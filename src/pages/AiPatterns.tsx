ead4acb4318b29ef027671cc4fc11a82a22af5c3
Asad Khan <asadkhan@Asads-MacBook-Pro-2.local>
2026-08-10 14:08:44 -0400

    random clamping stuff

--- src/pages/AiPatterns.tsx
+++ src/pages/AiPatterns.tsx
@@ -60,7 +60,8 @@ export default function AiPatterns() {
                     className= {styles.hero_image} 
                     style={{marginBottom: "16px"}} 
                     src="./ai-patterns-hero.webp"
-                    rel="preload"
+                    fetchPriority="high"
+                    decoding="sync"
                 >
                 </img>
                 <div className={styles.roles_and_timelines}>
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -419,7 +419,7 @@ function ProjectsListView({
                   fontSize: 'clamp(16px, 3.5vw, 20px)',
                   fontWeight: '500',
                   height: 'fit-content',
-                  lineHeight: 'clamp(26px, 2.5vw, 30px)',
+                  lineHeight: 'clamp(26px, 3.5vw, 30px)',
                   textAlign: 'left',
                   margin: 0,
                 }}
